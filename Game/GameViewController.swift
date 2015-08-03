aec558755cbeeba4175a63c6a10dd047ebb37c13
William Jaggers <17790@cgs.act.edu.au>
2015-08-03 10:03:29 +1000

    Deleted Comment

---
+++ Game/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Game
+//
+//  Created by William Jaggers on 3/06/2015.
+//  Copyright (c) 2015 William Jaggers. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
